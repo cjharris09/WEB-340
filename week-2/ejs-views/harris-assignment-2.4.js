53852f490389832ad3a9d6c12a1760c067631b9d
Chris Harris <cjharris9908@gmail.com>
2021-03-28 22:11:25 -0600

    comments
    Adding comments to this weeks work.

--- week-2/ejs-views/harris-assignment-2.4.js
+++ week-2/ejs-views/harris-assignment-2.4.js
@@ -1,19 +1,28 @@
+/*
+============================================
+; Title: Assignment 2.4 web 340
+; Author: Chris Harris
+; Date: 28 March 2021
+; 
+;===========================================
+*/
+
 var http = require('http');
 var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));// directory name to follow
+app.set('view engine', 'ejs');// use the ejs view engine
 app.get('/', function(request, response){
     response.render('index', {
-        firstName: "Chris",
-        lastName: "Harris",
-        address: "123 Richard Cheese Court"
+        firstName: "Chris",//this is the information that is being pulled and populated in the index.ejs file.
+        lastName: "Harris",//this is the information that is being pulled and populated in the index.ejs file.
+        address: "123 Richard Cheese Court"//this is the information that is being pulled and populated in the index.ejs file.
     });
 });
 
-http.createServer(app).listen(8080, function(){
+http.createServer(app).listen(8080, function(){//Creates server to run on port 8080.
     console.log('EJS-Views app started on port 8080');
 });
 
